bec3d4c952453a9a61c51e0e244405f7ba89236e
jaimoyok <jaimeyoldi2@gmail.com>
2022-05-17 14:37:35 +0200

    Logica del Juego

---
+++ app/src/main/java/eina/unizar/xiangqi_frontendmovil/GameLogic/Caballo.kt
@@ -0,0 +1,7 @@
+package eina.unizar.xiangqi_frontendmovil.GameLogic
+
+class Caballo (pos : Coordinates, board: PieceBoard, color: Color): PieceImpl(pos, board, color){
+    override fun getUpdateMoves(): List<Coordinates> {
+        TODO("Not yet implemented")
+    }
+}
